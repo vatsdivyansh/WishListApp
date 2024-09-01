969b679be2d965128822e6b35f064d3f3d7d3a3e
vatsdivyansh <vatsdivyansh4@gmail.com>
2024-09-02 05:28:00 +0530

    follow up changes

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -1,2 +1,42 @@
 package com.example.wishlistapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+@Composable
+fun AddEditDetailView(
+    id : Long ,
+    viewModel: WishViewModel ,
+    navController: NavController
+){
+    Scaffold(topBar = {AppBarView(title =
+        if(id != 0L) stringResource(id = R.string.update_wish)
+        else stringResource(id = R.string.add_wish)
+    )}){
+        Column( modifier = Modifier
+            .padding(it)
+            .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally ,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+@Composable
+fun WishTextField(label: String ,
+                  value: String ,
+                  onValueChanged: (String)->Unit){
+    
+
+}
