6c71cb8a1b8f69bd4dd310af9bebaa3bf05682c7
vatsdivyansh <vatsdivyansh4@gmail.com>
2024-09-02 04:54:27 +0530

    Naviagation prepared

--- app/src/main/java/com/example/wishlistapp/Navigation.kt
+++ app/src/main/java/com/example/wishlistapp/Navigation.kt
@@ -4,8 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
-fun Navigation(viewModel: WishViewModel = viewModel() ,navController: NavController = rememberNavController()){
+fun Navigation(viewModel: WishViewModel = viewModel() ,navController: NavHostController = rememberNavController()){
+    NavHost(navController = navController , startDestination = Screen.HomeScreen.route){
+
+    }
 
 }
