c17a9dc8e5480860084000a2f423dd6c9a1a1fd9
vatsdivyansh <vatsdivyansh4@gmail.com>
2024-09-01 22:48:08 +0530

    Navigation Icon is added to our app bar or top bar

--- app/src/main/java/com/example/wishlistapp/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -2,12 +2,20 @@
 package com.example.wishlistapp
 
 
+
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -19,6 +27,15 @@ fun AppBarView(
     title : String , // title that the AppBar or TopBar is going to have
     onBackNavClicked: () -> Unit = {}
 ){
+    val navigationIcon: (@Composable () -> Unit)? = {
+        // but why we're using nullable here i.e "?" because we want sometime that navigationIcon should not appear (when we,re at different screen) that time we want navigationIcaon composable to be null
+        IconButton(onClick  = {onBackNavClicked()}){
+           Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack ,
+               tint = Color.White ,
+               contentDescription = null)
+        }
+    } // and now use this navigationIcon in the navigationIcon composable of our TopAppBar
+
    TopAppBar(title = { Text(text = title ,
        color = colorResource(id = R.color.white),
        modifier = Modifier
@@ -28,7 +45,8 @@ fun AppBarView(
                             elevation = 3.dp, // elevation that we're adding to the app bar
                             backgroundColor = colorResource(id = R.color.app_bar_color), // app_bar_color is the color that I'm going to create by myself go to values->color.xml
 //                            navigationIcon =
-
+//                             navigationIcon is a composable so we can can create another conposable and then van pass it here
+                            navigationIcon = navigationIcon
 
        )
 
