f4794540d6e3816f8d8d6b2a02e3ee771611ae37
vatsdivyansh <vatsdivyansh4@gmail.com>
2024-09-03 02:56:00 +0530

    Navigae up functionality is added to BackIcon

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -38,7 +38,10 @@ fun AddEditDetailView(
     Scaffold(topBar = {AppBarView(title =
         if(id != 0L) stringResource(id = R.string.update_wish)
         else stringResource(id = R.string.add_wish)
-    )}){
+    ) {navController.navigateUp()}
+
+
+    }){
         Column( modifier = Modifier
             .padding(it)
             .wrapContentSize(),
